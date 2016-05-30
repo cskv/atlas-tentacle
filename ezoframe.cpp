71ffd68b8d746b2ca679fde2fc1e329d6be96f35
Paul van Kan <pjmvkan@xs4all.nl>
2016-05-30 13:11:35 +0200

    Added EZOFrame Deleted other stuff

---
+++ ezoframe.cpp
@@ -0,0 +1,160 @@
+#include "ezoframe.h"
+#include "ui_ezoframe.h"
+
+EZOFrame::EZOFrame(QWidget *parent) :
+    QFrame(parent),
+    ui(new Ui::EZOFrame)
+{
+    ui->setupUi(this);
+    ui->stateLed->setOnColor(Qt::blue);
+    ui->stateLed->setOffColor(Qt::gray);
+    ui->stateLed->setState(true);
+}
+
+EZOFrame::~EZOFrame()
+{
+    delete ui;
+}
+
+void EZOFrame::updateAll()
+{
+    //ui->btnLED->click();
+    //ui->btnSlope->click();
+    //ui->btnInfo->click();
+    //ui->btnStatus->click();
+
+    if ( ui->cbAuto->isChecked() ) {
+        ui->btnpH->click();
+    }
+    //ui->btnGetTemp->click();
+    //ui->btnCal->click();
+    displayAll();
+}
+
+void EZOFrame::displayAll()
+{
+    ui->stateLed->setState( tm1->getLedState() );
+
+    double dval = tm1->getAcidSlope();
+    if (dval > 0) ui->acidSlopeLabel->setText(QString::number(dval));
+    dval = tm1->getBasicSlope();
+    if (dval > 0) ui->basicSlopeLabel->setText(QString::number(dval));
+
+    ui->probeLabel->setText(tm1->getProbeType());
+    ui->versionLabel->setText(tm1->getVersion());
+
+    ui->rstLabel->setText(tm1->getRstCode());
+    dval = tm1->getVoltage();
+    if (dval > 0) ui->voltLabel->setText(QString::number(dval));
+
+    if ( ui->cbAuto->isChecked() ) {
+        dval = tm1->getCurrentpH();
+        if (dval > -1 && dval < 15) ui->pHLabel->setText(QString::number(dval, 'f', 2 ));
+    }
+
+    dval = tm1->getCurrentTemp();
+    if (dval > 0) ui->tempLabel->setText(QString::number(dval , 'f', 1 ));
+    int ival = tm1->getCalState();
+    if (ival > -1) ui->calLabel->setText(QString::number(ival));
+}
+
+void EZOFrame::on_btnGetTemp_clicked()
+{
+    lastCmd = tm1->readTemp();
+    //serial->write(lastCmd);
+}
+
+void EZOFrame::on_btnpH_clicked()
+{
+    lastCmd = tm1->readpH();
+    emit cmdAvailable(lastCmd);
+    //serial->write(lastCmd);
+}
+
+void EZOFrame::on_btnLED_clicked()
+{
+    lastCmd = tm1->readLED();
+    //serial->write(lastCmd);
+}
+
+void EZOFrame::on_btnSetTemp_clicked()
+{
+    lastCmd = tm1->writeTemp();
+    //serial->write(lastCmd);
+    QTimer::singleShot(300, this, SLOT(on_btnGetTemp_clicked()));
+}
+
+void EZOFrame::on_btnCal_clicked()
+{
+    lastCmd = tm1->readCal();
+    //serial->write(lastCmd);
+}
+
+void EZOFrame::on_btnCalClear_clicked()
+{
+    lastCmd = tm1->doCal(0);
+    //serial->write(lastCmd);
+    QTimer::singleShot(2000, this, SLOT(on_btnCal_clicked()));
+    ui->btnCalHigh->setEnabled(false);
+    ui->btnCalLow->setEnabled(false);
+}
+
+void EZOFrame::on_btnCalMid_clicked()
+{
+    lastCmd = tm1->doCal(1);
+    //serial->write(lastCmd);
+    QTimer::singleShot(2000, this, SLOT(on_btnCal_clicked()));
+    ui->btnCalHigh->setEnabled(true);
+    ui->btnCalLow->setEnabled(true);
+}
+
+void EZOFrame::on_btnCalLow_clicked()
+{
+    lastCmd = tm1->doCal(2);
+    //serial->write(lastCmd);
+    QTimer::singleShot(2000, this, SLOT(on_btnCal_clicked()));
+}
+
+void EZOFrame::on_btnCalHigh_clicked()
+{
+    lastCmd = tm1->doCal(3);
+    //serial->write(lastCmd);
+    QTimer::singleShot(2000, this, SLOT(on_btnCal_clicked()));
+}
+
+void EZOFrame::on_btnSlope_clicked()
+{
+    lastCmd = tm1->readSlope();
+    //serial->write(lastCmd);
+}
+
+void EZOFrame::on_btnInfo_clicked()
+{
+    lastCmd = tm1->readInfo();
+    //serial->write(lastCmd);
+}
+
+void EZOFrame::on_btnStatus_clicked()
+{
+    lastCmd = tm1->readStatus();
+    //serial->write(lastCmd);
+}
+
+void EZOFrame::on_ledCheckBox_clicked(bool checked)
+{
+    lastCmd = tm1->writeLED(checked);
+    //serial->write(lastCmd);
+    QTimer::singleShot(300, this, SLOT(on_btnLED_clicked()));
+}
+
+void EZOFrame::on_contCB_clicked(bool checked)
+{
+    if (checked) mainTimer->start(1000);
+    else mainTimer->stop();
+}
+
+void EZOFrame::on_btnSleep_clicked()
+{
+    lastCmd = tm1->sleep();
+    //serial->write(lastCmd);
+}
---
+++ ezoframe.h
@@ -0,0 +1,59 @@
+#ifndef EZOFRAME_H
+#define EZOFRAME_H
+
+#include <QFrame>
+
+#include <QTimer>
+
+#include "atlasdialog.h"
+#include "qatlas.h"
+#include "qatlasusb.h"
+#include "ledindicator.h"
+
+namespace Ui {
+class EZOFrame;
+}
+
+class EZOFrame : public QFrame
+{
+    Q_OBJECT
+
+public:
+    explicit EZOFrame(QWidget *parent = 0);
+    ~EZOFrame();
+
+    QByteArray lastCmd;
+
+signals:
+    void cmdAvailable(QByteArray newCommand);
+
+private slots:
+    void on_btnGetTemp_clicked();
+    void on_btnpH_clicked();
+    void on_btnSetTemp_clicked();
+    void on_btnCal_clicked();
+    void on_btnCalClear_clicked();
+    void on_btnCalMid_clicked();
+    void on_btnCalLow_clicked();
+    void on_btnCalHigh_clicked();
+
+    void updateAll();
+    void displayAll();
+    void on_btnSlope_clicked();
+    void on_btnInfo_clicked();
+    void on_btnStatus_clicked();
+    void on_ledCheckBox_clicked(bool checked);
+    void on_btnLED_clicked();
+    void on_contCB_clicked(bool checked);
+    void on_btnSleep_clicked();
+
+private:
+    Ui::EZOFrame *ui;
+    LedIndicator* ledStateLed;
+
+    QATLAS* tm1 = new QATLAS(99);
+    QATLAS* tm2 = new QATLAS(101);
+    QTimer* mainTimer;
+};
+
+#endif // EZOFRAME_H
